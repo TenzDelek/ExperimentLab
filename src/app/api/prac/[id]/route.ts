cbeddae5df4906ee3b1a4b7b09557bd82f288f68
TenzDelek <tibetdelek@gmail.com>
2024-09-18 22:51:27 +0530

    build pass (type error fixed)

--- src/app/api/prac/[id]/route.ts
+++ src/app/api/prac/[id]/route.ts
@@ -1,9 +1,16 @@
 import { NextRequest, NextResponse } from "next/server";
 import data from '../../../../utils/data.json'
-export async function GET(req:NextRequest,context:any) {
-    const {params}=context
-    const user=data.filter((user)=>user.id.toString()==params.id)
-    return NextResponse.json({
-        user
-    })
+
+interface RouteContext {
+  params: {
+    id: string
+  }
+}
+
+export async function GET(req: NextRequest, context: RouteContext) {
+  const { params } = context
+  const user = data.filter((user) => user.id.toString() === params.id)
+  return NextResponse.json({
+    user
+  })
 }
